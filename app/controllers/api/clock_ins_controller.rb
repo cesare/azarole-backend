f3950bcba4adec092ba9ce4a890fb0b85e2a0cb9
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-03 16:40:58 +0900

    Authenticate with API key

--- app/controllers/api/clock_ins_controller.rb
+++ app/controllers/api/clock_ins_controller.rb
@@ -1,6 +1,10 @@
 class Api::ClockInsController < ApplicationController
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate_with_api_key!
+
   def create
-    workplace = Workplace.find(params[:workplace_id])
+    workplace = current_user.workplaces.find(params[:workplace_id])
     attendance_record = workplace.attendance_records.create!(event: "clock-in")
 
     response_json = {
@@ -11,4 +15,15 @@ class Api::ClockInsController < ApplicationController
     }
     render json: response_json, status: :created
   end
+
+  private
+
+  def authenticate_with_api_key!
+    authenticate_or_request_with_http_token do |token, _options|
+      digest = ApiKey::TokenDigester.new(token:).digest
+      @api_key = ApiKey.includes(:user).find_by!(digest:)
+    end
+  end
+
+  def current_user = @api_key.user
 end
