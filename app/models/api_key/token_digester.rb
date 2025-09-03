8a53f2891eaa04254638f0b23b9d042be6c5c96c
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-03 17:20:20 +0900

    Move secret key into credentials

--- app/models/api_key/token_digester.rb
+++ app/models/api_key/token_digester.rb
@@ -11,5 +11,5 @@ class ApiKey::TokenDigester
 
   private
 
-  def secret_key = "fed0f073876b71b4c2b1df09290ac2e797bbb8262d25aebc452deb0690b34f35" # temporary value
+  def secret_key = Rails.application.credentials.api_key.digesting_secret_key
 end
