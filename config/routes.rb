da0a417360663c9aac9195b00cfb49e24f95ed41
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-06 16:08:31 +0900

    Handle authentication failures

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
   get "auth/google_oauth2/callback", to: "google_auth_sessions#create"
+  get "auth/failure", to: redirect("/")
   delete "signout", to: "sessions#destroy"
 
   namespace :api do
