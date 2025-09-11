50d0eac6370fc306f8dd2989d006fae1a70794d6
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-11 13:55:33 +0900

    Use action_cable/engine only in testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
+require "action_cable/engine" # rspec/rails requires this one
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 
