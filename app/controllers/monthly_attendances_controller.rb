e3b26d27b446269852f91b2d0395d5fdf7896654
SAWADA Tadashi <cesare@mayverse.jp>
2025-09-18 15:59:28 +0900

    Fix variable names

--- app/controllers/monthly_attendances_controller.rb
+++ app/controllers/monthly_attendances_controller.rb
@@ -5,23 +5,23 @@ class MonthlyAttendancesController < ApplicationController
     workplace = current_user.workplaces.find(params[:workplace_id])
     year = params[:year].to_i(10)
     month = params[:month].to_i(10)
-    work_times = MonthlyReportGenerator.new(workplace:, year:, month:).generate
-    monthly_working_times = MonthlyWorkingTime.new(year:, month:, working_times: work_times)
+    working_times = MonthlyReportGenerator.new(workplace:, year:, month:).generate
+    monthly_working_times = MonthlyWorkingTime.new(year:, month:, working_times:)
 
     response_json = {
       year: year,
       month: month,
-      dailyWorkTimes: monthly_working_times.map do |date, daily_work_times|
+      dailyWorkTimes: monthly_working_times.map do |date, daily_working_times|
         {
           day: date.day,
-          timeTracked: daily_work_times.time_tracked,
-          hasError: daily_work_times.has_error?,
-          workTImes: daily_work_times.map do |work_time|
+          timeTracked: daily_working_times.time_tracked,
+          hasError: daily_working_times.has_error?,
+          workTImes: daily_working_times.map do |working_time|
             {
-              complete: work_time.complete?,
-              startedAt: work_time.started_at&.iso8601,
-              endedAt: work_time.ended_at&.iso8601,
-              timeTracked: work_time.duration.round
+              complete: working_time.complete?,
+              startedAt: working_time.started_at&.iso8601,
+              endedAt: working_time.ended_at&.iso8601,
+              timeTracked: working_time.duration.round
             }
           end
         }
